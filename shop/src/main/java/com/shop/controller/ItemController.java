15da6889d44eab2c7996195d5f53a0214a057c17
imjeongyong@gmail.com <imjeongyong@gmail.com>
2023-03-07 17:05:29 +0900

    gogo

--- shop/src/main/java/com/shop/controller/ItemController.java
+++ shop/src/main/java/com/shop/controller/ItemController.java
@@ -1,10 +1,5 @@
 package com.shop.controller;
 
-import com.shop.dto.ItemSearchDto;
-import com.shop.entity.Item;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -13,10 +8,8 @@ import com.shop.dto.ItemFormDto;
 
 import com.shop.service.ItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import javax.persistence.EntityNotFoundException;
 import javax.validation.Valid;
 
 import org.springframework.validation.BindingResult;
@@ -24,6 +17,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+
+import javax.persistence.EntityNotFoundException;
+
+import com.shop.dto.ItemSearchDto;
+import com.shop.entity.Item;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
 import java.util.Optional;
 
 @Controller
@@ -99,16 +103,23 @@ public class ItemController {
     }
 
     @GetMapping(value = {"/admin/items", "/admin/items/{page}"})
-    public String itemManage(ItemSearchDto itemSearchDto,
-                             @PathVariable("page") Optional<Integer> page, Model model) {
-        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);
+    public String itemManage(ItemSearchDto itemSearchDto, @PathVariable("page") Optional<Integer> page, Model model) {
 
-        Page<Item> items =
-                itemService.getAdminItemPage(itemSearchDto, pageable);
-        itemService.getAdminItemPage(itemSearchDto, pageable);
+        Pageable pageable = PageRequest.of(page.isPresent() ? page.get() : 0, 3);
+        Page<Item> items = itemService.getAdminItemPage(itemSearchDto, pageable);
+
         model.addAttribute("items", items);
         model.addAttribute("itemSearchDto", itemSearchDto);
         model.addAttribute("maxPage", 5);
+
         return "item/itemMng";
     }
+
+    @GetMapping(value = "/item/{itemId}")
+    public String itemDtl(Model model, @PathVariable("itemId") Long itemId) {
+        ItemFormDto itemFormDto = itemService.getItemDtl(itemId);
+        model.addAttribute("item", itemFormDto);
+        return "item/itemDtl";
+    }
+
 }
--- shop/src/main/java/com/shop/repository/ItemRepositoryCustom.java
+++ shop/src/main/java/com/shop/repository/ItemRepositoryCustom.java
@@ -4,8 +4,12 @@ import com.shop.dto.ItemSearchDto;
 import com.shop.entity.Item;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import com.shop.dto.MainItemDto;
 
 public interface ItemRepositoryCustom {
 
     Page<Item> getAdminItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
+    Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable);
+
 }
